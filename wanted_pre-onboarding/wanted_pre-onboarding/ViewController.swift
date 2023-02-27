eba8eb65c7a50387180a641055a2c06fccc6a520
erun94510 <erun9414@icloud.com>
2023-02-27 15:38:16 +0900

    [Add] init

---
+++ wanted_pre-onboarding/wanted_pre-onboarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wanted_pre-onboarding
+//
+//  Created by Donghoon Bae on 2023/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
